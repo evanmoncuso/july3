44bd846cd9ca8ec42f20552c6ff321284029e670
Evan Moncuso <evan.moncuso@gmail.com>
2016-07-03 16:58:34 -0700

    Fix issue where date was displaying weirdly.

--- client/scripts/components/searchResults.jsx
+++ client/scripts/components/searchResults.jsx
@@ -3,11 +3,11 @@ const SearchResults = ({data}) => (
         <h3>You searched for: </h3>
         {data.map((item, index) => {
           item = JSON.parse(item);
-          let time = new Date(item.time);
+          let time = new Date(item.time).toString();
           return (
             <p key = {index} className = "searchResult">
               {item.text}
-              <span className="searchTime"> {time.toString('MMM dd')} </span>
+              <span className="searchTime"> {time.slice(0, time.length-15)} </span>
             </p>
           )
           })
